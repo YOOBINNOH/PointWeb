e4caa1a3b1c1a19a817c695332623c034ad3b191
YOOBINNOH <kuninyb@naver.com>
2023-02-20 10:30:57 +0900

    Initialize

---
+++ src/main/java/project/PointWeb/PointWebApplication.java
@@ -0,0 +1,13 @@
+package project.PointWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointWebApplication.class, args);
+	}
+
+}
