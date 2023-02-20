1d442b5487b8fc298cf684e1e2e267c09f70ddf8
YOOBINNOH <kuninyb@naver.com>
2023-02-20 12:11:24 +0900

    feat : Repository designs completed

---
+++ src/main/java/repository/ResignRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import domain.Resign;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResignRepository extends JpaRepository<Resign, Long> {
+}
