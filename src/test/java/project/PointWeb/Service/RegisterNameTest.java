213d24c39d752075c930e5fd738061980c12b780
YOOBINNOH <kuninyb@naver.com>
2023-02-22 12:05:17 +0900

    test : Add memberId equls test

---
+++ src/test/java/project/PointWeb/Service/RegisterNameTest.java
@@ -0,0 +1,39 @@
+package project.PointWeb.Service;
+
+
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import project.PointWeb.Domain.Member;
+import project.PointWeb.Dto.MemberRegisterDto;
+import project.PointWeb.Repository.MemberRepository;
+import project.PointWeb.controller.MemberController;
+
+
+@SpringBootTest
+@Transactional
+@Rollback
+public class RegisterNameTest {
+
+    @Autowired MemberController memberController;
+    @Autowired MemberRepository memberRepository;
+    @Autowired MemberService memberService;
+
+    @Test
+    void duplicate_check() {
+
+        MemberRegisterDto memberRegisterDto = new MemberRegisterDto();
+        memberRegisterDto.setMemberId("test");
+        memberRegisterDto.setMemberPw(1234L);
+        memberRegisterDto.setTeamId(1L);
+
+        Member member = new Member(memberRegisterDto.getMemberId(), memberRegisterDto.getMemberPw(), memberRegisterDto.getTeamId(), null);
+        memberService.save(member);
+
+        Assertions.assertThat(memberService.isname_test_check("test")).isEqualTo(false);
+
+    }
+}
