5f941b6cde8d28ea04b8d54507fd52723bf0d0f0
YOOBINNOH <kuninyb@naver.com>
2023-02-22 11:39:01 +0900

    test : Complete basic login test

---
+++ src/test/java/project/PointWeb/Service/LoginBasicTest.java
@@ -0,0 +1,89 @@
+package project.PointWeb.Service;
+
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import project.PointWeb.Domain.Member;
+import project.PointWeb.Dto.MemberLoginDto;
+import project.PointWeb.Dto.MemberRegisterDto;
+import project.PointWeb.Repository.MemberRepository;
+import project.PointWeb.controller.MemberController;
+
+import java.util.Optional;
+
+@SpringBootTest
+@Transactional
+@Rollback
+public class LoginBasicTest {
+
+    @Autowired MemberController memberController;
+    @Autowired MemberRepository memberRepository;
+    @Autowired MemberService memberService;
+
+
+    @Test
+    void 아이디_존재_체크_테스트() {
+
+        MemberRegisterDto memberRegisterDto = new MemberRegisterDto();
+        memberRegisterDto.setMemberId("idtc");
+        memberRegisterDto.setMemberPw(1234L);
+        memberRegisterDto.setTeamId(1L);
+
+        Member member = new Member(memberRegisterDto.getMemberId(), memberRegisterDto.getMemberPw(), memberRegisterDto.getTeamId(), null);
+        memberService.save(member);
+
+        MemberLoginDto memberLoginDto = new MemberLoginDto();
+
+        String loginId = "idtc";
+        memberLoginDto.setMemberId(loginId);
+        memberLoginDto.setMemberPw(null);
+
+        Optional<Member> result = memberRepository.findBymemberId(loginId);
+
+        Long id = result.get().getId();
+
+        try {
+            Member member1 = memberRepository.findByid(id);
+            Assertions.assertThat(member1.getMemberId()).isEqualTo("idtc");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Test
+    void 비밀번호_일치_체크_테스트() {
+
+        MemberRegisterDto memberRegisterDto = new MemberRegisterDto();
+        memberRegisterDto.setMemberId("pwtc");
+        memberRegisterDto.setMemberPw(1234L);
+        memberRegisterDto.setTeamId(1L);
+
+        Member member = new Member(memberRegisterDto.getMemberId(), memberRegisterDto.getMemberPw(), memberRegisterDto.getTeamId(), null);
+        memberService.save(member);
+
+        MemberLoginDto memberLoginDto = new MemberLoginDto();
+
+
+        memberLoginDto.setMemberId("pwtc");
+        memberLoginDto.setMemberPw(1234L);
+
+        Optional<Member> result = memberRepository.findBymemberId("pwtc");
+
+        Long id = result.get().getId();
+
+        try {
+            Member member1 = memberRepository.findByid(id);
+            Assertions.assertThat(member1.getMemberPw()).isEqualTo(1234L);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
