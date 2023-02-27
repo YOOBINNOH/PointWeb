3f8381fba740724017593d7d48a9e29ca3d06a3d
YOOBINNOH <kuninyb@naver.com>
2023-02-27 10:53:44 +0900

    feat : Add delete member

--- src/main/java/project/PointWeb/controller/AdminController.java
+++ src/main/java/project/PointWeb/controller/AdminController.java
@@ -5,7 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import project.PointWeb.Domain.Member;
 import project.PointWeb.Repository.MemberRepository;
+
+import java.util.Optional;
 
 @Controller
 
@@ -37,6 +43,29 @@ public class AdminController {
     public String resign(Model model){
         model.addAttribute("members",memberRepository.findAll());
         return "/admin/resign";
+    }
+
+
+
+    @PostMapping("/admin/member")
+    public String DeleteMember(@RequestParam("memberId") String memberId,Model model){
+       Optional<Member> id = memberRepository.findBymemberId(memberId);
+
+        if(id.isPresent()){
+            Member member = memberRepository.findByid(id.get().getId());
+            memberRepository.delete(member);
+            model.addAttribute("delete_success",memberId+" 회원을 삭제했습니다.");
+            return "admin/delete_success";
+        }
+        else{
+            model.addAttribute("delete_fail","등록되지 않은 회원입니다.");
+            return "admin/delete_fail";
+        }
+
+
+
+
+
     }
 
 }
